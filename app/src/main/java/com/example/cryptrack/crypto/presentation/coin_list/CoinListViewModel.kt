d0b6311cec473a6aebfa968b4199a91d03bb886a
Praneeth-10 <sp.parasa@gmail.com>
2026-01-20 23:46:49 -0500

    Adding Viewmodel

---
+++ app/src/main/java/com/example/cryptrack/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.cryptrack.crypto.presentation.coin_list
+
+import androidx.lifecycle.ViewModel
+import com.example.cryptrack.crypto.domain.CoinDataSource
+
+class CoinListViewModel(
+    private val coinDataSource: CoinDataSource
+) : ViewModel() {
+    
+}

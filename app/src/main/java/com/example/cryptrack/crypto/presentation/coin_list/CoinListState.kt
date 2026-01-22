d9083e1bec5bb82aa1c16a43247dbdad4589c10d
Praneeth-10 <sp.parasa@gmail.com>
2026-01-21 21:38:09 -0500

    Adding the loadCoins Function in init of Viewmodel

--- app/src/main/java/com/example/cryptrack/crypto/presentation/coin_list/CoinListState.kt
+++ app/src/main/java/com/example/cryptrack/crypto/presentation/coin_list/CoinListState.kt
@@ -5,7 +5,7 @@ import com.example.cryptrack.crypto.presentation.models.CoinUi
 
 @Immutable
 sealed interface CoinListState {
-    data object Loading : CoinListState
+    object Loading : CoinListState
     data class CoinList(val coinList : List<CoinUi> ) : CoinListState
     data class Error(val message: String) : CoinListState
     data class SelectedCoin(val selectedCoin : CoinUi? = null) : CoinListState
--- app/src/main/java/com/example/cryptrack/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/example/cryptrack/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -1,10 +1,57 @@
 package com.example.cryptrack.crypto.presentation.coin_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptrack.core.domain.util.onError
+import com.example.cryptrack.core.domain.util.onSuccess
 import com.example.cryptrack.crypto.domain.CoinDataSource
+import com.example.cryptrack.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class CoinListViewModel(
     private val coinDataSource: CoinDataSource
 ) : ViewModel() {
-    
+
+    private val _state = MutableStateFlow<CoinListState>(CoinListState.Loading)
+    val state: StateFlow<CoinListState> = _state.asStateFlow()
+
+    init {
+        loadCoins()
+    }
+
+    /**
+     * We load the coins or data only in view model not from any Activities or fragments.
+     * WE LOAD it when the View Model is initiated for that screen.
+     */
+    private fun loadCoins() {
+        viewModelScope.launch {
+
+            /**
+             * NOT Needed as we are already in the Loading Sate
+             */
+            _state.update {
+                CoinListState.Loading
+            }
+
+            coinDataSource.getCoins()
+                .onSuccess { coins ->
+                    _state.update {
+                        CoinListState.CoinList(coinList = coins.map {
+                            it.toCoinUi()
+                        })
+                    }
+                }
+                .onError { error ->
+                    _state.update {
+                        CoinListState.Error(message = error.name)
+                    }
+                }
+        }
+    }
+
+
 }

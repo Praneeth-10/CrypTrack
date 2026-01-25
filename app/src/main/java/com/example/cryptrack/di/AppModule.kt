699cf4a19fcba025f8d5437011db23ceaee9b033
Praneeth-10 <sp.parasa@gmail.com>
2026-01-24 23:34:11 -0500

    Added KOIN DI

--- app/src/main/java/com/example/cryptrack/di/AppModule.kt
+++ app/src/main/java/com/example/cryptrack/di/AppModule.kt
@@ -1,7 +1,21 @@
 package com.example.cryptrack.di
 
+import com.example.cryptrack.core.data.networking.HttpClientFactory
+import com.example.cryptrack.crypto.data.networking.RemoteCoinDataSource
+import com.example.cryptrack.crypto.domain.CoinDataSource
+import com.example.cryptrack.crypto.presentation.coin_list.CoinListViewModel
+import io.ktor.client.engine.cio.CIO
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val appModule = module {
+    single {
+        HttpClientFactory.create(CIO.create())
+    }
 
+    singleOf(::RemoteCoinDataSource).bind<CoinDataSource>()
+
+    viewModelOf(::CoinListViewModel)
 }

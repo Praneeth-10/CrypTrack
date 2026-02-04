262be7ed45b1d0ecf967937d3beedbabc4225896
Praneeth-10 <sp.parasa@gmail.com>
2026-02-03 23:53:55 -0500

    new Event handler

---
+++ app/src/main/java/com/example/cryptrack/core/presentation/util/ObserveAsEvents.kt
@@ -0,0 +1,14 @@
+package com.example.cryptrack.core.presentation.util
+
+import androidx.compose.runtime.Composable
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <T> ObserveAsEvents(
+    events : Flow<T>,
+    key1 : Any? = null,
+    key2 : Any? = null,
+    onEvent : (T) -> Unit
+) {
+
+}
